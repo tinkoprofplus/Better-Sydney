d87d895ad8f33295db78a1314ab2c3b106f2e5e7
Tinkoprof+ <151852963+tinkoprofplus@users.noreply.github.com>
2025-06-13 00:32:30 +0300

    Update CapesModule.java

--- src/main/java/me/aidan/sydney/modules/impl/core/CapesModule.java
+++ src/main/java/me/aidan/sydney/modules/impl/core/CapesModule.java
@@ -4,36 +4,14 @@ import lombok.Getter;
 import me.aidan.sydney.Sydney;
 import me.aidan.sydney.modules.Module;
 import me.aidan.sydney.modules.RegisterModule;
-import me.aidan.sydney.settings.impl.ModeSetting;
 import net.minecraft.util.Identifier;
 
 @Getter
 @RegisterModule(name = "Capes", description = "Applies the Sydney cape to yourself and to other users.", category = Module.Category.CORE, toggled = true, drawn = false)
 public class CapesModule extends Module {
-    public ModeSetting capeMode = new ModeSetting("Cape Mode", "Select which cape to display", "Sydney", new String[]{"Sydney", "Klux", "Future"});
-    private Identifier capeTexture;
-
-    @Override
-    public void onEnable() {
-        capeMode.onChange(capeMode -> updateCapeTexture());
-    }
-    
-    @Override
-    public void onDisable() {
-        capeMode.onChange(capeMode -> updateCapeTexture());
+    public CapesModule() {
+        this.capeTexture = Identifier.of(Sydney.MOD_ID, "textures/cape.png");
     }
 
-    public void updateCapeTexture() {
-        switch (capeMode.getValue().toLowerCase()) {
-            case "Sydney":
-                this.capeTexture = Identifier.of(Sydney.MOD_ID, "textures/cape.png");
-                break;
-            case "Klux":
-                this.capeTexture = Identifier.of(Sydney.MOD_ID, "textures/klux.png");
-                break;
-            case "Future":
-                this.capeTexture = Identifier.of(Sydney.MOD_ID, "textures/future.png");
-                break;
-        }
-    }
+    private final Identifier capeTexture;
 }
